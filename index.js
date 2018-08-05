f92838b26bfdf8bc516fd4fa9f9d032e80491afe
Alex Caswell <caswellyatsenko@gmail.com>
2018-08-05 05:48:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,19 +10,49 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  function randomnumber(){
+    return Math.random() * (100 - 1) + 1;
+  }
+ var cartlist = {itemName: item, itemPrice: randomnumber()}
+ cart.push(cartlist)
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  var L = getCart().length;
+  if (L === 0){
+    return "Your shopping cart is empty.";
+  }else{
+    var string = []
+    if (L === 1){
+      for(var a = 0; a < L; a++){
+        string.push(` ${getCart()[a].itemName} at $${getCart()[a].itemPrice}`)
+      }}else{
+        for(var b = 0; b < L - 1; b++){
+          string.push(` ${getCart()[b].itemName} at $${getCart()[b].itemPrice}`)
+        }for(var c = L - 1; c < L; c++){
+          string.push(` and ${getCart()[c].itemName} at $${getCart()[c].itemPrice}`)
+      }
+    }
+  }
+  return (`In your cart, you have${string}.`);
 }
+  
+
 
 function total() {
-  // write your code here
+  var cost = 0;
+  for(var i = 0; i < getCart().length; i++){
+    cost += parseFloat(getCart()[i].itemPrice);
+  }
+  return cost;
 }
 
-function removeFromCart(item) {
-  // write your code here
+function removeFromCart (item) {
+ ///first run a for loop on the entire getCart()
+ ///then an if else
+ ///if there is a matching item name then remove it and return the list
+ ///if not then return the other string
 }
 
 function placeOrder(cardNumber) {
